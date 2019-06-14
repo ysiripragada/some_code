3308f2c2e9c205ff4d0c60017863688dc660e7a6
Yashwanth Siripragada <ysiripragada@gmail.com>
2019-06-14 03:28:18 -0500

    Update testscript.sh
    Change of Variable Name!

--- testscript.sh
+++ testscript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "This script is from GitHub!"
-$NUMBER = $(($FIRSTNUMBER + $SECONDNUMBER))
+NUMBER = $(($FIRSTNUMBER + $SECONDNUMBER))
 echo "$NUMBER"
 echo "The Script is Completed!"
